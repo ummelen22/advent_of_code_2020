cd500755457dc77392faf8f69316560a949eff44
Alex Ummelen <aummelen@smart-robotics.nl>
2021-01-22 22:49:18 +0100

    remove std namespace, initialize ifstream with value and catch stoi exceptions

--- src/day1.cpp
+++ src/day1.cpp
@@ -4,28 +4,36 @@
 #include <vector>
 #include <map>
 
-using namespace std;
+std::vector<int> readPuzzleInputFromFile(const std::string &fileName) {
+    std::ifstream inFile(fileName);
+    std::string line;
+    std::vector<int> numbersInFile;
 
-vector<int> readPuzzleInputFromFile(string &fileName) {
-    ifstream inFile;
-    string line;
-    vector<int> numbersInFile;
-
-    inFile.open(fileName);
     while (getline(inFile, line)) {
-        numbersInFile.push_back(stoi(line));
+        try {
+            numbersInFile.push_back(stoi(line));
+        } 
+        catch (const std::invalid_argument& ia) {
+            std::cerr << "Invalid argument: " << line << " couldn't be converted to an int." << std::endl;
+        }
+        catch (const std::out_of_range& oor) {
+            std::cerr << "Invalid argument: " << line << " is out of range of representable values of int." << std::endl;
+        }
     }
-    inFile.close();
+
+    for (auto &it : numbersInFile) {
+        std::cout << it << std::endl;
+    }
 
     return numbersInFile;
 }
 
-int getProductOfKeysThatSumUpToX(map<int, int> &inputMap, int &sum, int &numberOfKeys) {
+int getProductOfKeysThatSumUpToX(std::map<int, int> &inputMap, int &sum, int &numberOfKeys) {
     for (auto const &it : inputMap) {
         int diff = sum - it.first;
         if (numberOfKeys > 2) {
             // Create temporary map to prevent the same key to be found in the recursive function call
-            map<int, int> tmpInputMap = inputMap;
+            std::map<int, int> tmpInputMap = inputMap;
             tmpInputMap.erase(it.first);
             int tmpNumberOfKeys = numberOfKeys - 1;
             if (int product = getProductOfKeysThatSumUpToX(tmpInputMap, diff, tmpNumberOfKeys)) {
@@ -46,11 +54,11 @@ int getProductOfKeysThatSumUpToX(map<int, int> &inputMap, int &sum, int &numberO
 
 int main() {
     // Read puzzle input from txt file
-    string fileName = "../inputs/day1.txt";
-    vector<int> inputArray = readPuzzleInputFromFile(fileName);
+    std::string fileName = "../inputs/day1.txt";
+    std::vector<int> inputArray = readPuzzleInputFromFile(fileName);
 
     // Create std::map, mapping the puzzle input integers to their indices (O(log n) time complexity for search)
-    map<int, int> inputMap;
+    std::map<int, int> inputMap;
     for (int i = 0; i < inputArray.size(); i++) {
         inputMap[inputArray[i]] = i;
     }
@@ -58,12 +66,12 @@ int main() {
     // Get user input
     int sum;
     int numberOfKeys;
-    cout << "Sum you are looking for: ";
-    cin >> sum;
-    cout << "Amount of numbers that should add up to " << sum << ": ";
-    cin >> numberOfKeys;
+    std::cout << "Sum you are looking for: ";
+    std::cin >> sum;
+    std::cout << "Amount of numbers that should add up to " << sum << ": ";
+    std::cin >> numberOfKeys;
 
     // Find product of keys recursively
     int product = getProductOfKeysThatSumUpToX(inputMap, sum, numberOfKeys);
-    cout << "Solution: " << product << endl;
+    std::cout << "Solution: " << product << std::endl;
 }
