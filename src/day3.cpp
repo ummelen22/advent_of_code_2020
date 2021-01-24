f73f7bc8ef20d0ee6df1e59209a9e91917099642
Alex Ummelen <aummelen@smart-robotics.nl>
2021-01-24 01:42:08 +0100

    solution day 3

---
+++ src/day3.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <unordered_set>
+
+
+std::vector<std::string> readPuzzleInputFromFile(std::string fileName) {
+    std::ifstream inFile(fileName);
+    std::string line;
+    std::vector<std::string> linesInFile;
+
+    while (getline(inFile, line)) {
+        linesInFile.push_back(line);
+    }
+    return linesInFile;
+}
+
+int numberOfTreesOnSlope(const std::vector<std::string>& area, std::vector<int> slope){
+    int index = 0;
+    int counter = 0;
+    int stepRight = slope[0];
+    int stepDown = slope[1];
+    for (std::vector<std::string>::const_iterator it = area.begin(); it != area.end() + stepDown - 1; it += stepDown) {
+            std::string line = *it;
+            if (line[index % line.length()] == '#') {
+                counter++;
+            }
+            index += stepRight;
+        }
+    return counter;
+}
+
+int calcProduct(const std::vector<std::string>& inputLines, const std::vector<std::vector<int>>& stepCombinations) {
+    
+    std::vector<int> numberOfTreesPerSlope = {};
+
+    for (std::vector<std::vector<int>>::const_iterator it = stepCombinations.begin(); it != stepCombinations.end(); it++) {
+        int numberOfTrees = numberOfTreesOnSlope(inputLines, *it);
+        numberOfTreesPerSlope.push_back(numberOfTrees);
+    } 
+
+    int product = 1;
+    for (auto& numberOfTrees : numberOfTreesPerSlope){
+        product *= numberOfTrees;
+    }
+
+    return product;
+}
+
+int main() {
+    std::vector<std::string> inputLines = readPuzzleInputFromFile("../inputs/day3.txt");
+
+    std::vector< std::vector<int> > slopesPart1 = {{3, 1}}; // slopes for part 1 {step right, step down}
+    std::vector< std::vector<int> > slopesPart2 = { // slopes for part 2 {step right, step down}
+        {1, 1},
+        {3, 1},
+        {5, 1},
+        {7, 1},
+        {1, 2}
+    };
+    
+    std::cout << "Solution part 1: " << calcProduct(inputLines, slopesPart1) << std::endl;
+    std::cout << "Solution part 2: " << calcProduct(inputLines, slopesPart2) << std::endl;
+}
