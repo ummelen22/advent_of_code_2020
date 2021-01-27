a658aea78b61be44ad942a357b68aa5c1249dcd0
Alex Ummelen <aummelen@smart-robotics.nl>
2021-01-28 00:08:41 +0100

    part 2: check years, height and haircolor

--- src/day4.cpp
+++ src/day4.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <unordered_map>
 #include <fstream>
+#include <regex>
 
 std::vector<std::string> splitString(std::string line, std::string delimiter){
     size_t pos = line.find(delimiter);
@@ -42,17 +43,46 @@ std::vector<std::unordered_map<std::string, std::string>> readPuzzleInputFromFil
     return passports;
 }
 
+bool endsWith(std::string fullString, std::string ending) {
+    if (fullString.length() >= ending.length()) {
+        return (fullString.compare(fullString.length() - ending.length(), ending.length(), ending) == 0);
+    } else {
+        return false;
+    }
+}
+
 bool isKeyValueValid(std::string key, std::string value){
-    if (key == "byr") {
-        try {
-            int byr = std::stoi(value);
-            if (byr >= 1920 && byr <= 2002) return true;
+    if (key.find("yr") != std::string::npos) {
+        try { // Validate years
+            int yr = std::stoi(value);
+            if (key == "byr" && yr >= 1920 && yr <= 2002) return true;
+            if (key == "iyr" && yr >= 2010 && yr <= 2020) return true;
+            if (key == "eyr" && yr >= 2020 && yr <= 2030) return true;
         }
         catch (std::invalid_argument) {
             std::cout << "Value " << value << " for key " << key << " could not be converted to int.\n";
         }
-        return false;
+    } else if (key == "hgt" ) {
+        // std::cout << value << std::endl;
+        std::string heightString = value;
+        try {
+            if (endsWith(value, "cm")) {
+                heightString.erase(heightString.length() - 2, 2);
+                int height = std::stoi(heightString);
+                if (height >= 150 && height <= 193) return true;
+            } else if (endsWith(value, "in")){
+                heightString.erase(heightString.length() - 2, 2);
+                int height = std::stoi(heightString);
+                if (height >= 59 && height <= 76) return true;
+            } else return false;
+        } catch (std::invalid_argument) {
+            std::cout << "Value " << value << " for key " << key << " could not be converted to int.\n";
+        }
+    } else if (key == "hcl") {
+        const std::regex hairColorConstraints("^#([a-f0-9]{6})$"); 
+        std::cout << value << " " << std::regex_match(value, hairColorConstraints) << std::endl;
     }
+    return false;
 }
 
 int main() {
@@ -65,7 +95,7 @@ int main() {
                 counter--;
                 break;
             } 
-            else if (key == "byr" ) counter -= (int)!isKeyValueValid(key, passport.at(key));
+            else if (key == "hgt" ) counter -= (int)!isKeyValueValid(key, passport.at(key));
         }
     }
 
