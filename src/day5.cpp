bc1fa07b68d61d1b0988866beaa8bf2c096c06cf
Alex Ummelen <aummelen@smart-robotics.nl>
2021-01-30 01:23:32 +0100

    solve day 5 part 2

--- src/day5.cpp
+++ src/day5.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <map>
+#include <limits.h>
 
 std::vector<std::string> readPuzzleInputFromFile(std::string fileName) {
     std::ifstream inFile(fileName);
@@ -39,17 +41,37 @@ int main() {
     // Read puzzle input from txt file
     std::string fileName = "../inputs/day5.txt";
     std::vector<std::string> inputArray = readPuzzleInputFromFile(fileName);
-    
-    int max_seat = 0;
+    std::map<int, std::vector<int>> rowCol{};
+    int maxSeat = 0;
     for (std::string& seating : inputArray) {
         std::string binarySeat = seatingToBinary(seating);
         int row = binaryToDecimal(binarySeat.substr(0, 7));
         int col = binaryToDecimal(binarySeat.substr(7, 10));
+        if (rowCol.find(row) != rowCol.end()) {
+            rowCol[row].push_back(col);
+        } else {
+            rowCol.insert(std::pair<int, std::vector<int>>(row, {col}));
+        }
 
         int seat = row * 8 + col;
-
-        if (seat > max_seat) max_seat = seat;
+        if (seat > maxSeat) maxSeat = seat;
     }
-    std::cout << "Solution Part 1: " << max_seat << std::endl;
+
+    int seat[2] = {0, 0};
+    int sum = 0;
+    for (auto& it : rowCol) {
+        if (it.second.size() == 7 && it.first != rowCol.begin()->first && it.first != rowCol.end()->first) {
+            for (auto& itCol : it.second) {
+                sum += itCol;
+            }
+            seat[0] = it.first;
+            seat[1] = 28 - sum;
+            break;
+        }
+    }
+    int mySeat = seat[0] * 8 + seat[1];
+
+    std::cout << "Solution Part 1: " << maxSeat << std::endl;
+    std::cout << "Solution Part 2: " << mySeat << std::endl;
     return 0;
 }
